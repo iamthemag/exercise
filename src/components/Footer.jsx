b6c367718efcdf0ef87d66598d8075215a01207d
mothilal77 <mothilallavudya@gmail.com>
2025-04-24 09:15:35 +0530

    Added files

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px"> ❤️ Made by Mothilal, Abdul Gafoor, Praveen ❤️</Typography>
+  </Box>
+);
+
+export default Footer;
