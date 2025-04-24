f32b15f6461d3e487f91457137af13b1b598c7f2
iamthemag <mohammedabdulgafoor2004@gmail.com>
2025-04-24 09:50:46 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 3000, // Dynamically use PORT env var or fallback to 3000
+    strictPort: true,  // Prevents Vite from using an auto-chosen port if 3000 is taken
+  },
 })
